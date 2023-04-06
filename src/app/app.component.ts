e30f3842b8a06697cd76781384f459be4f781c4e
E.Solaki <eleni.solaki@hotmail.com>
2023-04-06 14:03:55 +0300

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  name= 'Eleni Solaki'
   title = 'angular-exercise-0';
   words: string[] = [];
   isEven = true;
